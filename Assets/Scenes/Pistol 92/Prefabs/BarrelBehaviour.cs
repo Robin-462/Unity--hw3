2769bbffc99a2c21cd9b6e77034e7652d4e584ff
Robin-462 <ruochenzheng4@gmail.com>
2024-08-08 22:37:59 +0800

    assets

--- Assets/Scenes/Pistol 92/Prefabs/BarrelBehaviour.cs
+++ Assets/Scenes/Pistol 92/Prefabs/BarrelBehaviour.cs
@@ -2,20 +2,44 @@ using UnityEngine;
 
 public class BarrelBehaviour : MonoBehaviour
 {
-    private ParticleSystem Explosion;
+    private ParticleSystem explosionEffect;
 
     void Start()
     {
-        Explosion = GetComponentInChildren<ParticleSystem>();
-        Debug.Log("Explosion component found: " + (Explosion!= null));
+        explosionEffect = GetComponentInChildren<ParticleSystem>();
+        Debug.Log("Explosion component found: " + (explosionEffect != null));
+
+        Collider barrelCollider = GetComponent<Collider>();
+        if (barrelCollider != null)
+        {
+            barrelCollider.isTrigger = true;
+            Debug.Log("Barrel Collider is present and set as Trigger.");
+        }
+        else
+        {
+            Debug.LogError("No Collider found on the barrel.");
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Pistol Bullet")
+        Debug.Log("OnTriggerEnter method called");
+        Debug.Log("Collision with: " + other.gameObject.name);
+        if (other.gameObject.name.Contains("Pistol Bullet"))
         {
-            Explosion.Play();
-            Destroy(gameObject);
+            Debug.Log("Pistol Bullet hit the barrel!");
+
+            if (explosionEffect != null)
+            {
+                explosionEffect.Play();
+                explosionEffect.transform.parent = null;
+                Destroy(gameObject, 0.1f);
+                Destroy(explosionEffect.gameObject, explosionEffect.main.duration);
+            }
+            else
+            {
+                Debug.LogError("Explosion effect is missing!");
+            }
         }
     }
 }
